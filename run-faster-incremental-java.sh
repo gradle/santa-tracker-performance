19b1fc24a4409918ba2066d277648ae87ca4fc0a
Lóránt Pintér <lorant.pinter@gmail.com>
2020-03-04 18:13:15 +0100

    Allow to customize AGP and Kotlin plugin versions

--- run-faster-incremental-java.sh
+++ run-faster-incremental-java.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+AGP_VERSION="${AGP_VERSION:-3.6.1}"
+GRADLE_VERSION="${GRADLE_VERSION:-6.2.2}"
+
 sync
 
 gradle-profiler/gradlew --project-dir gradle-profiler install
 
 gradle-profiler/build/install/gradle-profiler/bin/gradle-profiler \
+    -DagpVersion=$AGP_VERSION \
     --project-dir santa-tracker-java \
     --scenario-file santa-tracker-java.scenarios \
     --gradle-version "$GRADLE_VERSION" \
--- run-faster-incremental-kotlin.sh
+++ run-faster-incremental-kotlin.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+AGP_VERSION="${AGP_VERSION:-3.6.1}"
+KOTLIN_VERSION="${KOTLIN_VERSION:-1.3.70}"
+GRADLE_VERSION="${GRADLE_VERSION:-6.2.2}"
+
 sync
 
 gradle-profiler/gradlew --project-dir gradle-profiler install
 
 gradle-profiler/build/install/gradle-profiler/bin/gradle-profiler \
+    -DagpVersion=$AGP_VERSION \
+    -DkotlinVersion=$KOTLIN_VERSION \
     --project-dir santa-tracker-kotlin \
     --scenario-file santa-tracker-kotlin.scenarios \
     --gradle-version "$GRADLE_VERSION" \
